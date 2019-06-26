aac972708f711d1a6065b5bdbfbf71418b8dc053
Thiago da Costa <thiago_frasao@hotmail.com.br>
2019-06-26 00:43:41 -0300

    merge de modificacoes

--- DispositivoHVAC.py
+++ DispositivoHVAC.py
@@ -5,6 +5,7 @@ Created on Mon Jun 10 23:22:55 2019
 @author: thiag
 """
 from Dispositivos import Dispositivo
+import numpy as np
 class DispositivoHVAC(Dispositivo):
     
     resTermica = 0
@@ -12,14 +13,20 @@ class DispositivoHVAC(Dispositivo):
     tempOut = 0
     capTermica = 0
     
-    def __init__(self,tipo,consumo,capTermica,resTermica,tempIn,tempOut):
-        Dispositivo.__init__(tipo,consumo)
+    def __init__(self,capTermica,resTermica,tempIn,tempOut):
+        Dispositivo.__init__(self,"HVAC",0)
+        self.capTermica = capTermica
         self.resTermica = resTermica
         self.tempIn = tempIn
         self.tempOut = tempOut
+
     
-    def calcularConsumo(self,tempoExecutando):
-        return (self.capTermica - ((self.tempIn - self.tempOut)/self.resTermica)) * tempoExecutando
+    def calcularConsumo(self,tempoExecutando,tempDesejada):
+        expoente = np.e**(-1*(tempoExecutando/(self.resTermica*self.capTermica)))
+        pri = self.tempIn * expoente
+        sec = self.resTermica*(1-expoente)
+        tec = self.tempOut*(1-expoente)
+        return (tempDesejada - pri - tec)/sec
     
     def setTempIn(self,novoTempIn):
         self.tempIn = novoTempIn
